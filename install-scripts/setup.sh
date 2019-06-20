57819fefb9a3a66edd76466a9bd378fc5347b051
Andrejus <git@andrejus.uk>
2019-06-21 00:18:08 +0100

    zsh syntax highlighting

--- install-scripts/setup.sh
+++ install-scripts/setup.sh
@@ -2,6 +2,10 @@
 export DEBIAN_FRONTEND=noninteractive
 
 
+# set up
+ln -s /mnt/c/git ~/git
+
+
 # # # shell
 
 # apt
@@ -22,14 +26,18 @@ sudo apt-get install powerline fonts-powerline -y
 # oh my zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$HOME/.zsh-syntax-highlighting" --depth 1
+echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+
 # spaceship
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/g' ~/.zshrc
 
 
-# # # dev
-
+# # # dev 
+brew install hugo
 
 # cleanup
 sudo apt autoremove -y
