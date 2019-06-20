5881ad56874647a5d18264e284c817f6b3264fda
Andrejus <git@andrejus.uk>
2019-06-21 00:11:51 +0100

    setup.sh formatting

--- install-scripts/setup.sh
+++ install-scripts/setup.sh
@@ -1,6 +1,9 @@
 # start
 export DEBIAN_FRONTEND=noninteractive
 
+
+# # # shell
+
 # apt
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -23,6 +26,10 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/g' ~/.zshrc
+
+
+# # # dev
+
 
 # cleanup
 sudo apt autoremove -y
