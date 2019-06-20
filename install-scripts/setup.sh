b5567b21fabf223fcf2091d68a06ada31f7efcd5
Andrejus <git@andrejus.uk>
2019-06-21 00:04:56 +0100

    zsh, oh-my-zsh, and spaceman

--- install-scripts/setup.sh
+++ install-scripts/setup.sh
@@ -12,6 +12,18 @@ eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 sudo apt-get install build-essential
 brew install gcc 
 
+# zsh
+sudo apt install zsh -y
+sudo apt-get install powerline fonts-powerline -y
+
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# spaceship
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/g' ~/.zshrc
+
 # cleanup
 sudo apt autoremove -y
 export DEBIAN_FRONTEND=readline
