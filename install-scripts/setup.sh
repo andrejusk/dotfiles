8d060d9b519c0cfb582309cc122347ca3055afd4
Andrejus <git@andrejus.uk>
2019-06-20 23:48:09 +0100

    Disable debian front-end during install

--- install-scripts/setup.sh
+++ install-scripts/setup.sh
@@ -1,3 +1,6 @@
+# start
+export DEBIAN_FRONTEND=noninteractive
+
 # apt
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -8,3 +11,6 @@ echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 sudo apt-get install build-essential
 brew install gcc 
+
+# cleanup
+export DEBIAN_FRONTEND=readline
