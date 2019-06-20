3871b9a0df34753c72d86d906abd60f4ead0cc16
Andrejus <git@andrejus.uk>
2019-06-20 23:50:08 +0100

    yes brew

--- install-scripts/setup.sh
+++ install-scripts/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # brew
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+yes '' | sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 sudo apt-get install build-essential
