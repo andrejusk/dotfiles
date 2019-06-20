5af70e4d01921406f5bc5aed764f8c3f5f5688dd
Andrejus <git@andrejus.uk>
2019-06-20 23:40:55 +0100

    Create setup.sh

---
+++ install-scripts/setup.sh
@@ -0,0 +1,10 @@
+# apt
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# brew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.profile
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+sudo apt-get install build-essential
+brew install gcc 
