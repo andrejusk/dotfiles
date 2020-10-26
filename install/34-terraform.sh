ae81a6be21b86ea34938caf7cbb34532e71e3379
Andrejus <hi@andrejus.uk>
2020-10-26 17:01:20 +0000

    Bump TF version and language server, coc.nvim fixes

--- install/34-terraform.sh
+++ install/34-terraform.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 source "$(dirname $0)/utils.sh"
 
+tf_version="0.13.5"
 if not_installed "terraform"; then
     echo "Installing terraform..."
-    wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
-    unzip terraform_0.12.24_linux_amd64.zip -d "$dotfiles_dir/tmp"
-    rm terraform_0.12.24_linux_amd64.zip
+    tf_archive="terraform_${tf_version}_linux_amd64.zip"
+    wget "https://releases.hashicorp.com/terraform/${tf_version}/${tf_archive}"
+    unzip "$tf_archive" -d "$dotfiles_dir/tmp"
+    rm "$tf_archive"
     mkdir -p ~/.local/bin
     mv "$dotfiles_dir/tmp/terraform" ~/.local/bin
     rm "$dotfiles_dir/tmp/terraform"
@@ -14,12 +16,16 @@ fi
 echo "terraform is installed"
 terraform --version
 
-if not_installed "terraform-lsp"; then
-    echo "Installing terraform-lsp..."
-    wget https://github.com/juliosueiras/terraform-lsp/releases/download/v0.0.11-beta2/terraform-lsp_0.0.11-beta2_linux_amd64.tar.gz
-    tar -C "$dotfiles_dir/tmp" -xzf terraform-lsp_0.0.11-beta2_linux_amd64.tar.gz
-    rm terraform-lsp_0.0.11-beta2_linux_amd64.tar.gz
-    mv "$dotfiles_dir/tmp/terraform-lsp" ~/.local/bin
+tf_lsp_version="0.8.0"
+if not_installed "terraform-ls"; then
+    echo "Installing terraform-ls..."
+    tf_lsp_archive="terraform-ls_${tf_lsp_version}_linux_amd64.zip"
+    wget "https://releases.hashicorp.com/terraform-ls/${tf_lsp_version}/${tf_lsp_archive}"
+    unzip "${tf_lsp_archive}" -d "$dotfiles_dir/tmp"
+    rm "${tf_lsp_archive}"
+    mkdir -p ~/.local/bin
+    mv "$dotfiles_dir/tmp/terraform-ls" ~/.local/bin
+    rm "$dotfiles_dir/tmp/terraform-ls"
 fi
 
 echo "terraform-lsp is installed"
