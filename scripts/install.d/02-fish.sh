38a8e81d69352db740bce9cd8d3d506d263bfae0
Andrejus <hi@andrejus.uk>
2021-05-02 23:57:48 +0100

    fix: CI

--- scripts/install.d/02-fish.sh
+++ scripts/install.d/02-fish.sh
@@ -5,12 +5,12 @@ current_shell=`grep "^$USER" /etc/passwd`
 current_shell=${current_shell##*:}
 fish_shell=`which fish`
 if [[ "$current_shell" != "$fish_shell" ]]; then
-    chsh -s "$fish_shell"
+    sudo usermod --shell "$fish_shell" "$USER"
 fi
 
 fisher_location="$XDG_CONFIG_HOME/fish/functions/fisher.fish"
 if ! [ -f $fisher_location ]; then
-    fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+    fish -c "curl -sL https://git.io/fisher | source && fisher update"
 fi
 
 fish -c "fisher update"
