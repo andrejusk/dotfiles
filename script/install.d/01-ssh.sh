b1dcd8d68dff0646f886340ecd6fa70489e97785
Andrejus <hi@andrejus.uk>
2023-10-24 00:42:39 +0100

    feat: ssh script tweaks

--- script/install.d/01-ssh.sh
+++ script/install.d/01-ssh.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-ssh_target="$HOME/.ssh"
-ssh_key="$ssh_target/id_ed25519"
-ssh_pub="$ssh_key.pub"
-if [ ! -f "$ssh_key" ]; then
-    ssh-keygen -t ed25519 -f "$ssh_key"
+
+# -----------------------------------------------------------------------------
+# Description:
+#   Print SSH key.
+#
+
+ssh_method="ed25519"
+
+ssh_target="${HOME}/.ssh"
+ssh_key="${ssh_target}/id_${ssh_method}"
+ssh_pub="${ssh_key}.pub"
+if [ ! -f $ssh_key ]; then
+    ssh-keygen \
+        -t $ssh_method \
+        -f $ssh_key \
+        -C "$(whoami)@$(hostname)-$(date -I)"
 fi
 
-cat "$ssh_pub"
+cat $ssh_pub
+
+unset ssh_method ssh_target ssh_key ssh_pub
