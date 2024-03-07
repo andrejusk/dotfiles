f638492ce63f1e4fed3accdba2de5da0a816b0a1
Andrejus <git@andrejus.uk>
2024-03-07 01:43:05 +0000

    feat: apt package filter

--- script/install.d/03-apt.sh
+++ script/install.d/03-apt.sh
@@ -13,9 +13,11 @@ if command -v apt-get &> /dev/null; then
     )
 
     sudo apt-get update
-    sudo apt-get install -qq "${apt_packages[@]}"
-    sudo apt-get autoremove
-    sudo apt-get autoclean
+
+    apt_packages=($(comm -13 <(printf "%s\n" "${apt_packages[@]}" | sort) <(dpkg --get-selections | awk '{print $1}' | sort)))
+    if [ ${#apt_packages[@]} -gt 0 ]; then
+        sudo apt-get install -qq "${apt_packages[@]}"
+    fi
 
     unset apt_packages
 fi
