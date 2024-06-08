67a10c0dd0cc6a570ae77aff25562333b403c68c
Andrejus <git@andrejus.uk>
2024-06-08 15:13:59 +0100

    feat: install betterdisplay

---
+++ script/install.d/73-betterdisplay.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Description:
+#   (macOS only) Install BetterDisplay.
+#
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    if ! brew list --cask betterdisplay &> /dev/null; then
+        brew install --cask betterdisplay
+    else
+        echo "BetterDisplay is already installed."
+    fi
+fi
