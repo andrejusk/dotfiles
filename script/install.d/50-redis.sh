ed89ec3af46ed0a4234cf07010fb3814493c5dd6
Andrejus <git@andrejus.uk>
2025-04-09 22:46:01 +0100

    fix: check if gpg keyrings exist

--- script/install.d/50-redis.sh
+++ script/install.d/50-redis.sh
@@ -7,7 +7,10 @@
 
 if ! command -v redis-client &>/dev/null; then
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
+        redis_keyring_path="/usr/share/keyrings/redis-archive-keyring.gpg"
+        if [[ ! -f "$redis_keyring_path" ]]; then
+            curl -fsSL https://packages.redis.io/gpg | sudo gpg --dearmor -o "$redis_keyring_path"
+        fi
         echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://packages.redis.io/deb $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/redis.list
         sudo apt-get install -qq redis
     elif [[ "$OSTYPE" == "darwin"* ]]; then
