ba421f8024b85bd5d96d25a9dbf33b87bda61039
Andrejus <git@andrejus.uk>
2025-05-31 22:47:29 +0100

    fix: redis-client binary

--- script/install.d/50-redis.sh
+++ script/install.d/50-redis.sh
@@ -5,7 +5,7 @@
 #   Install Redis.
 #
 
-if ! command -v redis-client &>/dev/null; then
+if ! command -v redis-cli &>/dev/null; then
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         redis_keyring_path="/usr/share/keyrings/redis-archive-keyring.gpg"
         if [[ ! -f "$redis_keyring_path" ]]; then
@@ -18,3 +18,5 @@ if ! command -v redis-client &>/dev/null; then
         brew install redis
     fi
 fi
+
+redis-cli --version
