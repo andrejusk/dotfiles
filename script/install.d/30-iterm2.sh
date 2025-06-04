0588165cfd49204faf283cec44078a5d02ef0f9c
Andrejus <git@andrejus.uk>
2025-06-04 17:17:25 +0100

    fix: log iterm2 completion

--- script/install.d/30-iterm2.sh
+++ script/install.d/30-iterm2.sh
@@ -9,6 +9,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     if ! brew list --cask iterm2 &>/dev/null; then
         brew install --cask iterm2
     fi
+    echo -e "${GREEN}iTerm2 installed successfully!${NC}"
 else
     echo -e "${YELLOW}Skipping: Not macOS${NC}"
 fi
