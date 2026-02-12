61bf7b366b8979768991013ee1221ba76ee50d94
Andrejus <git@andrejus.uk>
2026-02-12 23:00:45 +0000

    fix: ^F escape

--- files/home/.zsh/widgets.zsh
+++ files/home/.zsh/widgets.zsh
@@ -81,7 +81,7 @@ _dots_load_keybindings() {
         selection="$(rg --color=always --line-number --no-heading --hidden --glob '!.git' '' 2>/dev/null \
             | fzf --ansi --delimiter=: \
                   --preview 'head -n $((({2}+30))) {1} | tail -n 60' \
-                  --preview-window='right:60%')" || { zle reset-prompt; return; }
+                  --preview-window='right:60%')" || { zle -I; zle reset-prompt; return; }
         local file="${selection%%:*}"
         local line="${${selection#*:}%%:*}"
         BUFFER="${EDITOR:-vim} +${line} ${(q)file}"
